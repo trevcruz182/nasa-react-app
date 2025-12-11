a96a9db83e50d98079b9454042677a66240f7e70
trevcruz182 <trevorcruz182@gmail.com>
2025-12-11 01:13:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nasa-react-app
+Build a ReactJS app with the Nasa API
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main(props) {
+    const {data} = props
+
+    return(
+        <div className="imgContainer">
+            <img className="bgImage" src={data?.hdurl} alt={data?.title} />
+        </div>
+    )
+}
